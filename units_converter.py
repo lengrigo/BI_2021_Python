74edfa6885fa1939d8705b0e40a3cd48ba292f78
lengrigo <lena_grigo99@mail.ru>
2021-10-14 22:49:07 +0700

    deleting wrong file

--- units_converter.py
+++
@@ -1,9 +0,0 @@
-# This program will convert concentration from mol/l into gram/l
-print("Enter concentration in mol/l: ")
-concentration_mol = float(input())
-print("Enter number of nucleotides: ")
-num = int(input())
-if concentration_mol < 0 or num < 0:
-    print("Concentration and amount of nucleotides are always positive numbers")
-concentration_gram = round(concentration_mol / (345 * num), 4)
-print("Concentration of oligonucleotide is ", concentration_gram, "gram/l")

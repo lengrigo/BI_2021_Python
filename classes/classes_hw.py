ef677d9b01bcd283cc71ced2e63f7671a2e51811
Elena Grigoreva <72381640+lengrigo@users.noreply.github.com>
2022-02-14 03:58:16 +0700

    flake8 corrections

--- classes/classes_hw.py
+++ classes/classes_hw.py
@@ -3,6 +3,7 @@ from Bio import Seq
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 
+
 # task 1
 class BioinformaticsStudent:
     def __init__(self, name, course, city, skills=None):
@@ -12,6 +13,7 @@ class BioinformaticsStudent:
         self.name = name
         self.course = course
         self.city = city
+    
     def subjects_semester1(self):
         bio_subjects = ['Discrete math,',
                         'Statistics and R,',
@@ -26,6 +28,7 @@ class BioinformaticsStudent:
             print(*bio_subjects)
         elif self.course == 'Algorithmic bioinformatics':
             print(*programmer_subjects)
+    
     def subjects_semester2(self):
         bio_subjects = ['Introduction to ML,',
                         'Python programming,',
@@ -43,13 +46,16 @@ class BioinformaticsStudent:
             print(*bio_subjects)
         elif self.course == 'Algorithmic bioinformatics':
             print(*programmer_subjects)
+    
     def learning(self, new_skill):
         self.skills.add(new_skill)
+    
     def meeting(self, student):
         if self.city == student.city:
             print(f'{self.name} and {student.name} can meet offline in {self.city}!')
         else:
             print(f'{self.name} and {student.name} can meet only online')
+    
     def skills_show(self):
         print(f'{self.name} can', *self.skills)
 
@@ -58,10 +64,12 @@ class BioinformaticsStudent:
 class Rna:
     def __init__(self, sequence):
         self.sequence = Seq.Seq(sequence)
+    
     def translation(self):
         translated_sequence = self.sequence.translate()
         return translated_sequence
-    def reverse_transcription (self):
+    
+    def reverse_transcription(self):
         dna = self.sequence.back_transcribe()
         return dna
 
@@ -75,21 +83,26 @@ class PositiveSet(set):
             for i in args:
                 if i > 0:
                     super(PositiveSet, self).add(i)
-    def add (self, *args):
+    
+    def add(self, *args):
         for number in args:
             if number > 0:
                 super(PositiveSet, self).add(number)
+
 
 # task 4
 class Fasta:
     def __str__(self):
         return self.path
     __repr__ = __str__
+    
     def __init__(self, path):
         self.path = path
-        self.sequences = [sequence.seq for sequence in SeqIO.parse(path,'fasta')]
+        self.sequences = [sequence.seq for sequence in SeqIO.parse(path, 'fasta')]
+    
     def seq_amount(self):
         return len(self.sequences)
+    
     def length_hist(self):
         lengths = [len(sequence) for sequence in self.sequences]
         fig, axis = plt.subplots(figsize=(15, 7))
@@ -97,9 +110,11 @@ class Fasta:
         plt.xlabel("Sequence length")
         plt.ylabel("Sequences count")
         plt.show()
+    
     def gc_count(self):
         gc_count = [GC(sequence) for sequence in self.sequences]
         return gc_count
+    
     def four_mers(self):
         four_mer = {}
         for sequence in self.sequences:
@@ -118,9 +133,10 @@ class Fasta:
         fig, axis = plt.subplots(figsize=(15, 7))
         axis.bar(x=list(range(len(four_mer))), height=four_mer.values())
         axis.set_xticks(range(0, len(four_mer), 1))
-        axis.set_xticklabels(four_mer.keys(), rotation = 90)
+        axis.set_xticklabels(four_mer.keys(), rotation=90)
         plt.xlabel("4-mer")
         plt.ylabel("4-mer count")
         plt.show()
+    
     def metrics(self):
         return self.seq_amount(), self.gc_count()
